99fde98a87608ae3358ac603e62699560e5c937f
Dvergar <laurent.rd@gmail.com>
2014-11-30 12:12:44 +0100

    first commit

---
+++ src/podstream/SerializerMacro.hx
@@ -0,0 +1,206 @@
+package podstream;
+import haxe.macro.Expr;
+import haxe.macro.Context;
+
+typedef NetworkVariable = {name:String, type:String, redirection:String};
+
+
+class SerializerMacro
+{
+    static public var componentIds:Int = -1;
+    static public var serialized:Array<String> = new Array();
+
+    static inline function getComponentId():Int
+    {
+        componentIds++;
+        return componentIds;
+    }
+
+    static public function getSerialized():Array<String>
+    {
+        trace("arrserialized " + serialized);
+        trace("serialized " + haxe.Resource.getString("serialized"));
+
+        var serializedSerialized:String = haxe.Resource.getString("serialized");
+        // if(serializedSerialized == null)
+        //     serializedSerialized = [];
+        return haxe.Unserializer.run(serializedSerialized);
+    }
+
+    #if macro
+    static public function _build(fields:Array<Field>):Array<Field>
+    {
+        var cls = Context.getLocalClass().get();
+        var pos = Context.currentPos();
+
+        var componentName = cls.name;
+        trace("component " + componentName);
+
+        // PROCESS EACH PARAMETER AND SAVE IT UP
+        var networkVariables:Array<NetworkVariable> = new Array();
+        for(f in fields)
+        {
+            if(f.meta.length != 0)
+            {
+                // META NET TYPES (can't be above because typemeta related to msgtypesmetas)
+                for(m in f.meta)
+                {
+                    if(m.name == "short" ||
+                       m.name == "int" ||
+                       m.name == "float" ||
+                       m.name == "bool" ||
+                       m.name == "byte" ||
+                       m.name == "string")
+                    {
+                        trace("m " + m);
+                        var netVar:NetworkVariable = {name:f.name,
+                                                      type:m.name,
+                                                      redirection:null};
+
+                        if(m.params.length != 0)
+                        {
+                            trace("param " + m.params[0].expr);
+                            switch(m.params[0].expr)
+                            {
+                                case EConst(CString(redirection)):
+                                    trace("redirection " + redirection);
+                                    netVar.redirection = redirection;
+                                case _:
+                            }
+                        }
+
+                        networkVariables.push(netVar);
+                    }
+                    else
+                    {
+                        Context.error("Not a network type : " + m.name, pos);
+                    }
+                }
+            }
+        }
+
+        haxe.macro.Context.onGenerate(function(types)
+        {
+            trace("onGenerate " + haxe.io.Bytes.ofString(haxe.Serializer.run(serialized)));
+            Context.addResource("serialized", haxe.io.Bytes.ofString(haxe.Serializer.run(serialized)));
+        });
+        
+        if(networkVariables.length == 0)
+        {
+            trace("No serialization, abort");
+            return fields;
+        }
+
+
+        ////////////////////////////////////////
+        // RETURN HERE PLEASE DONT FORGET HIM :'('
+        ////////////////////////////////////////
+
+        // ASSIGN ID
+        var id = getComponentId();
+
+        // ADDS ID TO OBJECT & CLASS
+        fields.push({kind: FVar(TPath({name: "Int", pack: [], params: [] }),
+                                      {expr: EConst(CInt(Std.string(id))), pos : pos }),
+                     meta: [], name: "_sid", doc: null, pos: pos, access: [APublic] });
+
+        fields.push({kind: FVar(TPath({name: "Int", pack: [], params: [] }),
+                                      {expr: EConst(CInt(Std.string(id))), pos : pos }),
+                     meta: [], name: "__sid", doc: null, pos: pos, access: [APublic, AStatic] });
+
+        // ADD COMPONENT TO ARRAY
+        serialized.push(componentName);
+        trace("hum : " + SerializerMacro.serialized);
+
+        trace("networkVariables " + networkVariables);
+
+        var inExprlist = [];
+        var outExprlist = [];
+
+        for(netVar in networkVariables)
+        {
+            var varNameOut = netVar.name;
+            var varNameIn = netVar.name;
+            if(netVar.redirection != null) varNameIn = netVar.redirection;
+            var varType = netVar.type;
+
+            var ein;
+            var eout;
+
+            trace("varType " + varType);
+            trace("varName " + varNameIn + " / " + varNameOut);
+
+            switch(varType)
+            {
+                case "short":
+                    outExprlist.push( macro bo.writeInt16(Std.int($i{varNameOut})) );
+                    inExprlist.push( macro $i{varNameIn} = bi.readInt16() );
+                case "int":
+                    outExprlist.push( macro bo.writeInt32(Std.int($i{varNameOut})) );
+                    inExprlist.push( macro $i{varNameIn} = bi.readInt32() );
+                case "byte":
+                    outExprlist.push( macro bo.writeByte(Std.int($i{varNameOut})) );
+                    inExprlist.push( macro $i{varNameIn} = bi.readByte() );
+                case "float":
+                    outExprlist.push( macro bo.writeFloat($i{varNameOut}) );
+                    inExprlist.push( macro $i{varNameIn} = bi.readFloat() );
+                case "bool":
+                    outExprlist.push( macro ($i{varNameOut} == true) ? bo.writeByte(1) : bo.writeByte(0) );
+                    inExprlist.push( macro $i{varNameIn} = (bi.readByte() == 0) ? return false : return true );
+                case "string":
+                    outExprlist.push( macro bo.writeInt16($i{varNameOut}.length) );
+                    outExprlist.push( macro bo.writeString($i{varNameOut}) );
+                    inExprlist.push( macro $i{varNameIn} = bi.readString(bi.readInt16()) );
+            }
+        }
+
+        // IN
+        var arg = {name:"bi", type:null, opt:false, value:null};
+
+        var func = {args:[arg],
+                    ret:null,
+                    params:[],
+                    expr:{expr:EBlock(inExprlist), pos:pos}};
+
+        fields.push({name: "unserialize",
+                     doc: null,
+                     meta: [],
+                     access: [APublic],
+                     kind: FFun(func),
+                     pos: pos});
+
+        // OUT
+        var arg = {name:"bo", type:null, opt:false, value:null};
+
+        var func = {args:[arg],
+                    ret:null,
+                    params:[],
+                    expr:{expr:EBlock(outExprlist), pos:pos}};
+
+        fields.push({name: "serialize",
+                     doc: null,
+                     meta: [],
+                     access: [APublic],
+                     kind: FFun(func),
+                     pos: pos});
+
+        for(f in fields)
+        {
+            trace("Serialized : " + new haxe.macro.Printer().printField(f));
+        }
+
+        return fields;
+    }
+    #end
+
+    macro static public function build():Array<haxe.macro.Field>
+    {
+        var fields = Context.getBuildFields();
+
+
+        fields = _build(fields);
+
+        return fields;
+    }
+
+}
